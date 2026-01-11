6fff4eba81992c99a350db7952c389842562621d
lizbeth181 <paolajacuinde57@gmail.com>
2026-01-11 13:37:48 -0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\UserController; // <--- IMPORTANTE: Importamos tu controlador
+use App\Http\Controllers\UserController; // <--- IMPORTANTE: Importamos controlador
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
